96c2c4bde38ba2e83c023e1cc3f209fac1eb6c77
94farooqui <94.farooqui@gmail.com>
2024-12-27 16:20:06 +0400

    added contact info and social media section in new hotel form but functionality needs to be added

--- client/src/pages/admin/AddNewHotel.jsx
+++ client/src/pages/admin/AddNewHotel.jsx
@@ -4,6 +4,17 @@ import { addHotel } from "../../api/hotel";
 import { IoClose } from "react-icons/io5";
 import { CiSquarePlus } from "react-icons/ci";
 
+import { IoCallOutline } from "react-icons/io5";
+import { IoLogoWhatsapp } from "react-icons/io5";
+import { IoMailOutline } from "react-icons/io5";
+import { IoEarth } from "react-icons/io5";
+
+import { IoLogoFacebook } from "react-icons/io5";
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaXTwitter } from "react-icons/fa6";
+import { IoLogoLinkedin } from "react-icons/io5";
+
+
 const roomTypesData = [
   "Deluxe",
   "Premium",
@@ -15,23 +26,28 @@ const roomTypesData = [
 const AddNewHotel = () => {
   const navigate = useNavigate();
   const [showNewRoomTypeModel, setShowNewRoomTypeModal] = useState(false);
-  const [newRoomType,setNewRoomType] = useState("")
+  const [newRoomType, setNewRoomType] = useState("");
+  const [showContactInfoTabs, setShowContactInfoTabs] = useState({
+    call: true,
+    whatsapp: false,
+    email: false,
+    website: false,
+  });
   const [newHotel, setNewHotel] = useState({
     name: "",
     location: "",
     coordinates: "",
     description: "",
+    contactinfo: [{ call: "", whatsapp: "", email: "", website: "" }],
     roomTypes: [],
   });
 
   const addroomType = (e) => {
-    if ((e.target.value == "new")) {
+    if (e.target.value == "new") {
       setShowNewRoomTypeModal(true);
-    } else if(newHotel.roomTypes.includes(e.target.value)){
-      return 
-    }
-    else {
-     
+    } else if (newHotel.roomTypes.includes(e.target.value)) {
+      return;
+    } else {
       const newRoomTypes = [...newHotel.roomTypes, e.target.value];
       setNewHotel({ ...newHotel, roomTypes: newRoomTypes });
     }
@@ -39,21 +55,22 @@ const AddNewHotel = () => {
 
   const handleAddHotel = async (e) => {
     e.preventDefault();
-    try {
-      const addedHotel = await addHotel(newHotel);
-      if (addedHotel) {
-        console.log(addedHotel);
-        setHotels([...hotels, addedHotel.data]);
-        setNewHotel({
-          name: "",
-          location: "",
-          description: "",
-          coordinates: "",
-        });
-      }
-    } catch (error) {
-      console.error("Error adding hotel:", error);
-    }
+    console.log("Form submitted");
+    // try {
+    //   const addedHotel = await addHotel(newHotel);
+    //   if (addedHotel) {
+    //     console.log(addedHotel);
+    //     setHotels([...hotels, addedHotel.data]);
+    //     setNewHotel({
+    //       name: "",
+    //       location: "",
+    //       description: "",
+    //       coordinates: "",
+    //     });
+    //   }
+    // } catch (error) {
+    //   console.error("Error adding hotel:", error);
+    // }
   };
 
   const handleDeleteHotel = async (id) => {
@@ -65,13 +82,21 @@ const AddNewHotel = () => {
     }
   };
 
+  const handleAddContactinfo = (e) => {
+    setNewHotel({
+      ...newHotel,
+      contactinfo: { ...newHotel.contactinfo, [e.target.name]: e.target.value },
+    });
+    console.log(newHotel);
+  };
+
   const handleAddNewRoomType = () => {
-    console.log("Room Type Added")
+    console.log("Room Type Added");
     const newRoomTypes = [...newHotel.roomTypes, newRoomType];
     setNewHotel({ ...newHotel, roomTypes: newRoomTypes });
-    setNewRoomType("")
-    setShowNewRoomTypeModal(false)
-  }
+    setNewRoomType("");
+    setShowNewRoomTypeModal(false);
+  };
 
   return (
     <div>
@@ -145,7 +170,6 @@ const AddNewHotel = () => {
             />
           </div>
         </div>
-
         <input
           type="text"
           placeholder="Description"
@@ -155,6 +179,9 @@ const AddNewHotel = () => {
           }
           className="rounded-md p-2 border mr-2"
         />
+      </form>
+      {/* Room Types Form */}
+      <form className="mt-4 mb-6 flex flex-col gap-4 rounded-md overflow-hidden">
         <div className="flex items-center gap-4 mt-4">
           <h2 className="text-xl font-semibold text-gray-800">Room Types</h2>
         </div>
@@ -210,11 +237,265 @@ const AddNewHotel = () => {
                 className="flex items-center group gap-2 px-4 border rounded-md py-2 shadow-sm font-semibold text-gray-600 bg-gradient-to-r from-gray-100 to-white"
               >
                 <p>{room}</p>
-                <button onClick={()=>setNewHotel({...newHotel, roomTypes: newHotel.roomTypes.filter(e => e!= room)})}>
+                <button
+                  onClick={() =>
+                    setNewHotel({
+                      ...newHotel,
+                      roomTypes: newHotel.roomTypes.filter((e) => e != room),
+                    })
+                  }
+                >
                   <IoClose className="hidden group-hover:block" />
                 </button>
               </div>
             ))}
+        </div>
+      </form>
+      {/* Contact Info Form */}
+      <div className="flex items-center gap-4 mt-4">
+        <h2 className="text-xl font-semibold text-gray-800">Contact info</h2>
+      </div>
+      <form className="mt-4 mb-6 flex flex-col gap-4 rounded-md overflow-hidden">
+        <div className="flex gap-x-4 items-center">
+          <button
+            type="button"
+            onClick={(e) =>
+              setShowContactInfoTabs({
+                ...showContactInfoTabs,
+                call: !showContactInfoTabs.call,
+              })
+            }
+            className={`px-4 py-2 rounded-md bg-gradient-to-r border shadow-sm ${
+              showContactInfoTabs.call
+                ? "from-gray-700 to-gray-600 text-white "
+                : "from-gray-100 to-white border"
+            } `}
+          >
+            <IoCallOutline />
+          </button>
+          <button
+            type="button"
+            onClick={(e) =>
+              setShowContactInfoTabs({
+                ...showContactInfoTabs,
+                whatsapp: !showContactInfoTabs.whatsapp,
+              })
+            }
+            className={`px-4 py-2 rounded-md bg-gradient-to-r border shadow-sm ${
+              showContactInfoTabs.whatsapp
+                ? "from-gray-700 to-gray-600 text-white "
+                : "from-gray-100 to-white border"
+            } `}
+          >
+            <IoLogoWhatsapp />
+          </button>
+          <button
+            type="button"
+            onClick={(e) =>
+              setShowContactInfoTabs({
+                ...showContactInfoTabs,
+                email: !showContactInfoTabs.email,
+              })
+            }
+            className={`px-4 py-2 rounded-md bg-gradient-to-r border shadow-sm ${
+              showContactInfoTabs.email
+                ? "from-gray-700 to-gray-600 text-white "
+                : "from-gray-100 to-white border"
+            } `}
+          >
+            <IoMailOutline />
+          </button>
+          <button
+            type="button"
+            onClick={(e) =>
+              setShowContactInfoTabs({
+                ...showContactInfoTabs,
+                website: !showContactInfoTabs.website,
+              })
+            }
+            className={`px-4 py-2 rounded-md bg-gradient-to-r border shadow-sm ${
+              showContactInfoTabs.website
+                ? "from-gray-700 to-gray-600 text-white "
+                : "from-gray-100 to-white border"
+            } `}
+          >
+            <IoEarth />
+          </button>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          {showContactInfoTabs.call && (
+            <div className="border rounded-md w-full flex items-center">
+              <input
+                name="call"
+                defaultValue={newHotel.contactinfo.call}
+                onChange={handleAddContactinfo}
+                className="p-2  w-full"
+                placeholder="Call"
+              />
+
+              <IoCallOutline className="text-lg text-gray-400 mr-4" />
+            </div>
+          )}
+          {showContactInfoTabs.whatsapp && (
+            <div className="border rounded-md w-full flex items-center">
+              <input
+                name="whatsapp"
+                defaultValue={newHotel.contactinfo.whatsapp}
+                onChange={handleAddContactinfo}
+                className="p-2  w-full"
+                placeholder="WhatsApp"
+              />
+              <IoLogoWhatsapp className="text-lg text-gray-400 mr-4" />
+            </div>
+          )}
+          {showContactInfoTabs.email && (
+            <div className="border rounded-md w-full flex items-center">
+              <input
+                name="email"
+                defaultValue={newHotel.contactinfo.email}
+                onChange={handleAddContactinfo}
+                className="p-2  w-full"
+                placeholder="Email"
+              />
+              <IoMailOutline className="text-lg text-gray-400 mr-4" />
+            </div>
+          )}
+          {showContactInfoTabs.website && (
+            <div className="border rounded-md w-full flex items-center">
+              <input
+                name="website"
+                defaultValue={newHotel.contactinfo.website}
+                onChange={handleAddContactinfo}
+                className="p-2  w-full"
+                placeholder="Website"
+              />
+              <IoEarth className="text-lg text-gray-400 mr-4" />
+            </div>
+          )}
+        </div>
+      </form>
+      {/* Social Media Form */}
+      {/* IoLogoFacebook IoLogoInstagram FaXTwitter IoLogoLinkedin */}
+      <div className="flex items-center gap-4 mt-4">
+        <h2 className="text-xl font-semibold text-gray-800">Social Media</h2>
+      </div>
+      <form className="mt-4 mb-6 flex flex-col gap-4 rounded-md overflow-hidden">
+        <div className="flex gap-x-4 items-center">
+          <button
+            type="button"
+            onClick={(e) =>
+              setShowContactInfoTabs({
+                ...showContactInfoTabs,
+                call: !showContactInfoTabs.call,
+              })
+            }
+            className={`px-4 py-2 rounded-md bg-gradient-to-r border shadow-sm ${
+              showContactInfoTabs.call
+                ? "from-gray-700 to-gray-600 text-white "
+                : "from-gray-100 to-white border"
+            } `}
+          >
+            <IoLogoFacebook />
+          </button>
+          <button
+            type="button"
+            onClick={(e) =>
+              setShowContactInfoTabs({
+                ...showContactInfoTabs,
+                whatsapp: !showContactInfoTabs.whatsapp,
+              })
+            }
+            className={`px-4 py-2 rounded-md bg-gradient-to-r border shadow-sm ${
+              showContactInfoTabs.whatsapp
+                ? "from-gray-700 to-gray-600 text-white "
+                : "from-gray-100 to-white border"
+            } `}
+          >
+            <IoLogoInstagram />
+          </button>
+          <button
+            type="button"
+            onClick={(e) =>
+              setShowContactInfoTabs({
+                ...showContactInfoTabs,
+                email: !showContactInfoTabs.email,
+              })
+            }
+            className={`px-4 py-2 rounded-md bg-gradient-to-r border shadow-sm ${
+              showContactInfoTabs.email
+                ? "from-gray-700 to-gray-600 text-white "
+                : "from-gray-100 to-white border"
+            } `}
+          >
+            <FaXTwitter />
+          </button>
+          <button
+            type="button"
+            onClick={(e) =>
+              setShowContactInfoTabs({
+                ...showContactInfoTabs,
+                website: !showContactInfoTabs.website,
+              })
+            }
+            className={`px-4 py-2 rounded-md bg-gradient-to-r border shadow-sm ${
+              showContactInfoTabs.website
+                ? "from-gray-700 to-gray-600 text-white "
+                : "from-gray-100 to-white border"
+            } `}
+          >
+            <IoLogoLinkedin />
+          </button>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          {showContactInfoTabs.call && (
+            <div className="border rounded-md w-full flex items-center">
+              <input
+                name="call"
+                defaultValue={newHotel.contactinfo.call}
+                onChange={handleAddContactinfo}
+                className="p-2  w-full"
+                placeholder="Call"
+              />
+
+              <IoLogoFacebook className="text-lg text-gray-400 mr-4" />
+            </div>
+          )}
+          {showContactInfoTabs.whatsapp && (
+            <div className="border rounded-md w-full flex items-center">
+              <input
+                name="whatsapp"
+                defaultValue={newHotel.contactinfo.whatsapp}
+                onChange={handleAddContactinfo}
+                className="p-2  w-full"
+                placeholder="WhatsApp"
+              />
+              <IoLogoInstagram className="text-lg text-gray-400 mr-4" />
+            </div>
+          )}
+          {showContactInfoTabs.email && (
+            <div className="border rounded-md w-full flex items-center">
+              <input
+                name="email"
+                defaultValue={newHotel.contactinfo.email}
+                onChange={handleAddContactinfo}
+                className="p-2  w-full"
+                placeholder="Email"
+              />
+              <FaXTwitter className="text-lg text-gray-400 mr-4" />
+            </div>
+          )}
+          {showContactInfoTabs.website && (
+            <div className="border rounded-md w-full flex items-center">
+              <input
+                name="website"
+                defaultValue={newHotel.contactinfo.website}
+                onChange={handleAddContactinfo}
+                className="p-2  w-full"
+                placeholder="Website"
+              />
+              <IoLogoLinkedin className="text-lg text-gray-400 mr-4" />
+            </div>
+          )}
         </div>
       </form>
     </div>
